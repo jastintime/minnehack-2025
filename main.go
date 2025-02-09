ce49dc748e91b25f34de7735fa835534fbf25744
jastintime <130672671+jastintime@users.noreply.github.com>
2025-02-08 23:09:48 -0600

    Updated dated, got rid of example data.

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
-package main 
+package main
 
-import "github.com/gin-gonic/gin" 
+import "github.com/gin-gonic/gin"
 import "github.com/gin-contrib/cors"
+import "os"
+import "encoding/json"
+import "log"
 
 func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
-	//serve static files in /images as /assets/$FILE_NAME 
+	//serve static files in /images as /assets/$FILE_NAME
 	r.Static("/assets", "./images")
-	r.GET("api/locations", getLocations)
+	content, err := os.ReadFile("database.json")
+	if err != nil {
+		panic(err)
+	}
+	if ok := json.Valid([]byte(content)); ok == false {
+		log.Fatalf("database.json is not a valid json file")
+	}
+	r.StaticFile("api/locations", "database.json")
+	//	r.GET("api/locations", getLocations)
 	r.Run()
 }
-
-
-
-
-
