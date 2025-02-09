45aed534db2a7a453e7f7e1365cadac0b2fcd5ad
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-02-08 23:46:15 -0600

    stuff and things

--- dialog.js
+++ dialog.js
@@ -1,16 +1,15 @@
-const dialog = document.querySelector(`dialog`);
+const dialog = document.querySelector(`#location`);
 const dialogCloseButton = document.querySelector(`.close`);
 dialogCloseButton.addEventListener(`click`, () => dialog.close());
 
-const carousel = document.getElementById(`carousel`);
-
 export function showModalLocation(location) {
    console.log(`location:`, location);
+   const carousel = document.getElementById(`carousel`);
    const images = location.images;
    const imageElements = images.map(img =>
-   `<swiper-slide>
+   `<swiper-slide class="image-container">
             <h1 class="title">${img.name}</h1>
-            <img src="${img.location}">
+            <img src="http://localhost:8080/${img.location}">
             <p class="description">${img.description}</p>
    </swiper-slide>`);
    carousel.outerHTML = `<swiper-container id="carousel" navigation="true">
--- index.js
+++ index.js
@@ -6,12 +6,15 @@ import { showModalLocation } from "./dialog.js";
 const minneapolis = [44.986656, -93.258133];
 const oldMap = `https://mapwarper.net/maps/tile/89404/{z}/{x}/{y}.png`;
 const osm = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const osmAttr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const satellite = `https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}`;
 // Create a map and center it on minneapolis
 const map = L.map('map').setView(minneapolis, 13);
 
-L.tileLayer(osm, {
+L.tileLayer(satellite, {
    maxZoom: 19,
-   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+   attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+   ext: 'jpg'
 }).addTo(map);
 
 // Add tiles
@@ -22,7 +25,7 @@ L.tileLayer(oldMap, {
 
 async function showLocations() {
    const data = await getLocations();
-   console.log(data);
+   console.log(`data we have:`, data);
    for (const loc of data) {
       addMarker(loc.coordinates, loc);
    }
@@ -33,4 +36,11 @@ function addMarker(location, context)  {
    pin.on(`click`, showModalLocation.bind(window, context))
 }
 
-// showLocations();
+const aboutDialog = document.querySelector('#about');
+const aboutCloseButton = document.querySelector('#about-close');
+const aboutButton = document.querySelector('#about-button');
+
+aboutButton.addEventListener(`click`, () => aboutDialog.showModal());
+aboutCloseButton.addEventListener(`click`, () => aboutDialog.close());
+
+showLocations();
