adbc601de287784b5378a9b89311fe144fa04a27
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-02-08 22:06:34 -0600

    blah blah

--- imageData.js
+++ imageData.js
@@ -1,3 +1,27 @@
 /*
 Image data element
 */
+
+export default class imageElement extends HTMLDivElement {
+   constructor() {
+      super();
+   }
+
+   render() {
+      this.innerHTML = `
+      <h1>${this.getAttribute(`data-title`)}</h1>
+      <img src="${this.getAttribute(`data-src`)}">
+      <p>${this.getAttribute(`data-description`)}</p>
+      `
+   }
+
+   connectedCallback() {
+      Tthis.render();
+   }
+
+   attributeChangedCallback() {
+      this.render();
+   }
+}
+
+customElements.define(`carousel-image`, imageElement);
