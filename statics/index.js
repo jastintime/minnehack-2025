ce68c0df0016dd538e8090b3d309d8462d9cd9ef
jastintime <130672671+jastintime@users.noreply.github.com>
2025-03-12 22:50:04 -0500

    Added boundary around minneapolis and st paul.
    Changed basemap provider to ESRI as they have a more generous free tier.

--- statics/index.js
+++ statics/index.js
@@ -1,97 +1,87 @@
-
 import * as L from "https://unpkg.com/leaflet/dist/leaflet-src.esm.js";
-import { getLocations,} from "./network.js";
+import { getLocations } from "./network.js";
 import { showModalLocation } from "./dialog.js";
+import "./BoundaryCanvas.js"
 
-const oldMap = `https://mapwarper.net/maps/tile/89429/{z}/{x}/{y}.png`;
-const osm = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-const osmAttr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-const satellite = `https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}`;
+const oldMap = 'https://mapwarper.net/maps/tile/89429/{z}/{x}/{y}.png';
+const satellite = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+// make account here https://wiki.openstreetmap.org/wiki/Esri#Interested_in_using_Esri_World_Imagery_in_your_own_application?
 
 var customIcon = L.icon({
-   iconUrl: './assets/custom_pin.png',
-
-   iconSize:     [20, 28], // size of the icon
-   iconAnchor:   [22, 25], // point of the icon which will correspond to marker's location
-
+	iconUrl: './assets/custom_pin.png',
+	iconSize: [20, 28], // size of the icon
+	iconAnchor: [22, 25], // point of the icon which will correspond to marker's location
 });
 
-// thank you for the rough params for what we needed to set for our map coords!
-// https://www.waldrn.com/apps/mspbuildingmap/index.html
-
 const map = L.map('map', {
-   center: [44.973368,-93.159883],
-   zoom: 11,
-   // minZoom: 10,
-   maxZoom: 15,
-   scrollWheelZoom: true,
-   maxBounds: [[44.0, -94.5],[46.0, -92.0]],
-   maxBoundsViscosity: .7
-})
+	zoom: 10,
+	maxZoom: 100,
+	maxBoundsViscosity: .7,
+	minZoom: 12,
+});
 
-// // // TODO: these cords don't work, and i don't wanna spend 30min goosechasing how to line em up perfectly 
-// const map = L.map('map').setView([45.01323589435436, -93.31595003819945], 13);
+// load our geojson bounds into a json object and once we receive that 
+// Promise we setup our map around that boundary
+var cityB = fetch('./minneapolis_and_stpaul.geojson').then(function(response) {
+	return response.json();
+}).then(function(cityB) {
+	var geojsonLayer = L.geoJSON(cityB, {
+		"fillOpacity": 0
+	}).addTo(map);
+	const bounds = geojsonLayer.getBounds();
+	map.fitBounds(bounds);
+	map.setMaxBounds(bounds);
 
-// // POSSIBLE BOUNDS CHECKING IMPLEMENT
-// // this code will stop the user from dragging the map outside the bounds of minneapolis
-// // it comes from here: https://stackoverflow.com/questions/25741532/disable-drag-once-attained-maximum-bounds-in-leaflet
-// // tysm artyom!
-// var bounds = L.latLngBounds([[44.986656, -93.258133], [44.95058896557884, -93.24742388864514]]);
-// map.setMaxBounds(bounds);
-// map.on('drag', function() {
-// 	map.panInsideBounds(bounds, { animate: false });
-// });
+	var satelliteLayer = L.TileLayer.boundaryCanvas(satellite, {
+		maxZoom: 19,
+		attribution: 'Esri, TomTom, Garmin, FAO, NOAA, USGS, © OpenStreetMap contributors, and the GIS User Community',
+		ext: 'jpg',
+		boundary: cityB,
+	}).addTo(map);
 
+	// the historic map we stitched together
+	var oldLayer = L.tileLayer(oldMap, {
+		maxZoom: 19,
+		attribution: '&copy; Regents of the University of Minnesota | &copy; <a href="https://apps.lib.umn.edu/mhapo/" target="_blank">UMN-MHAPO</a>',
+	}).addTo(map);
 
-
-// // Create a map and center it on minneapolis
-// const map = L.map('map').setView(minneapolis, 13);
-
-var osmLayer = L.tileLayer(satellite, {
-   maxZoom: 19,
-   attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-   ext: 'jpg'
-}).addTo(map);
-
-// Add tiles
-var oldLayer = L.tileLayer(oldMap, {
-   maxZoom: 19,
-   attribution: '&copy; Regents of the University of Minnesota | &copy; <a href="https://apps.lib.umn.edu/mhapo/" target="_blank">MHAPO</a> &copy; <a href="https://apps.lib.umn.edu/mhapo/" target="_blank">UMN-MHAPO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-
-}).addTo(map);
-
-var baseMaps = {
-	"Current": osmLayer,
-}
-
-var overlayMap = {
-	"1957": oldLayer,
-}
-
-var layerControl = L.control.layers(baseMaps,overlayMap).addTo(map);
+	// Layer control
+	var baseMaps = {
+		"Current": satelliteLayer,
+	};
+	var overlayMap = {
+		"1957": oldLayer,
+	};
+	var layerControl = L.control.layers(baseMaps, overlayMap).addTo(map);
+});
 
 
 
 
-
+// Function to show locations
 async function showLocations() {
-   const data = await getLocations();
-   console.log(`data we have:`, data);
-   for (const loc of data) {
-      addMarker(loc.coordinates, loc);
-   }
+	const data = await getLocations();
+	console.log("data we have:", data);
+	for (const loc of data) {
+		addMarker(loc.coordinates, loc);
+	}
 }
 
-function addMarker(location, context)  {
-   const pin = L.marker(location , {icon: customIcon}).addTo(map);
-   pin.on(`click`, showModalLocation.bind(window, context))
+// Function to add a marker
+function addMarker(location, context) {
+	const pin = L.marker(location, {
+		icon: customIcon
+	}).addTo(map);
+	pin.on('click', showModalLocation.bind(window, context));
 }
 
+// Modal handling
 const aboutDialog = document.querySelector('#about');
 const aboutCloseButton = document.querySelector('#about-close');
 const aboutButton = document.querySelector('#about-button');
 
-aboutButton.addEventListener(`click`, () => aboutDialog.showModal());
-aboutCloseButton.addEventListener(`click`, () => aboutDialog.close());
+aboutButton.addEventListener('click', () => aboutDialog.showModal());
+aboutCloseButton.addEventListener('click', () => aboutDialog.close());
 
+// Show locations after the map has loaded
 showLocations();
